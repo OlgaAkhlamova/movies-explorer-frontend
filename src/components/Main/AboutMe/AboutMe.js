f3ed0d4f525f0fe1fa3f590659d226567960a35a
Olga Akhlamova <olga-akhlamova@yandex.ru>
2022-11-20 18:56:09 +0300

    add section AboutMe

---
+++ src/components/Main/AboutMe/AboutMe.js
@@ -0,0 +1,39 @@
+import React from "react";
+import studentPhoto from "../../../images/student-photo.jpg";
+import "./AboutMe.css";
+
+function AboutMe() {
+  return (
+    <section className="about-me">
+      <h2 className="about-me__title">
+        Студент
+      </h2>
+      <div className="about-me__container">
+        <div className="about-me__left-column">
+          <h3 className="about-me__subtitle">Ольга</h3>
+          <p className="about-me__prof">
+            Фронтенд - разработчик
+          </p>
+          <p className="about-me__text">
+            Я родилась и живу в Москве, закончилa факультет электронной техники МЭИ.
+            Есть семья. Я люблю слушать музыку, мастерить, а ещё увлекаюсь разнообразными путешествиями.
+            Недавно начала кодить. После того, как прошла курс по веб-разработке, начала заниматься 
+            фриланс-заказами и ушла с постоянной работы.
+          </p>
+          <a className="about-me__link" href="https://github.com/OlgaAkhlamova" target="_blanc">
+            Github
+          </a>
+        </div>
+        <div className="about-me__right-column">
+          <img 
+            className="about-me__photo"
+            src={studentPhoto} 
+            alt="фото студента" 
+          />
+        </div>   
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
